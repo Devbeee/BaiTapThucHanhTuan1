0db53ee2f2895c9ec1a8ab134f82deb51c611487
leethanh1 <96030445+leethanh1@users.noreply.github.com>
2024-03-14 09:33:51 +0700

    lab01

---
+++ src/Bai3/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./App.module.css";
+class Header extends React.Component {
+    render() {
+        return <h1 className={styles.header}>Danh Lam Thắng Cảnh</h1>;
+    }
+}
+
+export default Header;
