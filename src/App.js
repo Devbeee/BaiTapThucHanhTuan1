08663ccd2de39982e56cbc148568e9c636bb1df6
QuocDuy03 <quocduyy.03@gmail.com>
2024-03-16 08:43:55 +0700

    complete bai1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
-
+import { Route, Routes } from "react-router-dom";
+import Bai1 from "./components/Bai1";
 function App() {
   return (
     <div>
-     
+      <Routes>
+      <Route path="/bai1" element={<Bai1/>}></Route>
+    </Routes>
     </div>
   );
 }
