179f1cf31e97f1df329e15a3df2865067c651f17
Devbeee <tngb1002@gmail.com>
2024-03-17 13:53:55 +0700

    fix bai3

--- src/Bai3/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from "./App.module.css";
-class Header extends React.Component {
-    render() {
-        return <h1 className={styles.header}>Danh Lam Thắng Cảnh</h1>;
-    }
-}
-
-export default Header;
